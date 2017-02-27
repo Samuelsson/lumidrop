ccd4a8ccf56e863a1dadc87afa003560be0ab1c7
Samuelsson <erik.samuelsson@gmail.com>
2017-02-27 01:13:45 +0100

    Migrate to Webpack 2
    Mainly just updated Webpack to the latest version and fix new
    configuration options in the config files.
    
    Some minor changes in addition to this are:
    
    * Updated loaders and related node packades to latest versions.
    * Split production and development configs into separate files.
    * Using an experimental version of UglifyJS so Babel isn't needed.

---
+++ webpack_config/webpack.config.dev.js
@@ -0,0 +1,20 @@
+'use strict';
+
+process.env.NODE_ENV = 'dev';
+
+const webpack = require('webpack');
+const base = require('./webpack.config.base');
+
+base.devtool = 'cheap-eval-source-map';
+
+base.plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+);
+
+base.devServer = {
+    contentBase: './dist',
+    stats: 'minimal',
+    hot: true
+};
+
+module.exports = base;
