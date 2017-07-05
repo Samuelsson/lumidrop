d3ae4257ed8de837f2fe1f239842f35a7bb4934a
Samuelsson <erik.samuelsson@gmail.com>
2017-07-05 22:52:24 +0200

    Add new main font

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+    navItems;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.navItems = [
+            'home',
+            'movies',
+            'statistics'
+        ]
+    }
+
+    ngOnInit() {
+    }
 
 }
