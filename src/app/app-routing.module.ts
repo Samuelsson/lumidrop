5791c771dae09abe89ff201903194f486564549b
Samuelsson <erik.samuelsson@gmail.com>
2017-07-05 21:29:37 +0200

    Move sections to their own folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { StatisticsComponent } from './statistics/statistics.component';
-import { NoContentComponent } from './no-content/no-content.component';
+import { HomeComponent } from './sections/home/home.component';
+import { StatisticsComponent } from './sections/statistics/statistics.component';
+import { NoContentComponent } from './sections/no-content/no-content.component';
+import { MoviesComponent } from './sections/movies/movies.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'statistics', component: StatisticsComponent },
+    { path: 'movies', component: MoviesComponent },
     { path: '**', component: NoContentComponent }
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    declarations: [
+        HomeComponent,
+        StatisticsComponent,
+        NoContentComponent,
+        MoviesComponent
+    ]
 })
 export class AppRoutingModule {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,10 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { StatisticsComponent } from './statistics/statistics.component';
-import { NoContentComponent } from './no-content/no-content.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        HomeComponent,
-        StatisticsComponent,
-        NoContentComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
